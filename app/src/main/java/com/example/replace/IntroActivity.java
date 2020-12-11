c663cc099eed0e9e85bac826a28a40b6171caf5c
lazaros7 <lazos.7@hotmail.com>
2020-12-11 22:59:03 +0200

    Some basic stuff

---
+++ app/src/main/java/com/example/replace/IntroActivity.java
@@ -0,0 +1,31 @@
+package com.example.replace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+
+    Button skipButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        skipButton = (Button) findViewById(R.id.buttonSkip);
+        skipButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openNewActivity();
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/replace/MainActivity.java
+++ app/src/main/java/com/example/replace/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.replace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView myImageView= (ImageView)findViewById(R.id.imageLogo);
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        myImageView.startAnimation(myFadeInAnimation); //Set animation to your ImageView
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
