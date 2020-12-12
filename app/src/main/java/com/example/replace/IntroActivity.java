b1e782acfcadd1575d60eccda9367b4a0564f7f2
lazaros7 <lazos.7@hotmail.com>
2020-12-12 03:44:26 +0200

    I did the slide with the right images.

--- app/src/main/java/com/example/replace/IntroActivity.java
+++ app/src/main/java/com/example/replace/IntroActivity.java
@@ -1,20 +1,34 @@
 package com.example.replace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class IntroActivity extends AppCompatActivity {
 
     Button skipButton;
+    ViewPager mViewPager;
+    ViewPagerAdapter mViewPagerAdapter;
+    TabLayout tabLayout;
+    int[] images = {R.drawable.first_image, R.drawable.second_image, R.drawable.third_image, R.drawable.fourth_image, R.drawable.fifth_image, R.drawable.sixth_image};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        mViewPager = (ViewPager)findViewById(R.id.viewPagerMain);
+        mViewPagerAdapter = new ViewPagerAdapter(IntroActivity.this, images);
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
 
         skipButton = (Button) findViewById(R.id.buttonSkip);
         skipButton.setOnClickListener(new View.OnClickListener(){
@@ -22,6 +36,7 @@ public class IntroActivity extends AppCompatActivity {
                 openNewActivity();
             }
         });
+
     }
 
     public void openNewActivity(){
