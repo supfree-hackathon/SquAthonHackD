44ba6d1bff688ed814dc01acb53fc2d60f3430da
Stefanos Tsotras <steftsotras2@gmail.com>
2020-12-12 20:07:44 +0200

    game integration

--- app/src/main/java/com/example/replace/PointGainActivity.java
+++ app/src/main/java/com/example/replace/PointGainActivity.java
@@ -1,14 +1,27 @@
 package com.example.replace;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PointGainActivity extends AppCompatActivity {
 
+    private Button gamebutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.points_gain_main);
+
+        gamebutton = (Button) findViewById(R.id.button);
+        gamebutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(PointGainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
