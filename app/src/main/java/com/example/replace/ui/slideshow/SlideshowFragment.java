38181873b06760a1c9e85de87cfc52cee1d7aa05
lazaros7 <lazos.7@hotmail.com>
2020-12-13 14:29:59 +0200

    last changes (maybe)

--- app/src/main/java/com/example/replace/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/replace/ui/slideshow/SlideshowFragment.java
@@ -28,13 +28,6 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         scanButton = (Button) root.findViewById(R.id.scanButton);
         scanButton.setOnClickListener(new View.OnClickListener() {
